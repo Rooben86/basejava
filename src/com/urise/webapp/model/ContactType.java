340c53c40d4d91f25109d6736693f688dd5c89ac
Ruben <rooben86@gmail.com>
2023-12-05 10:05:06 +0300

    hw7

---
+++ src/com/urise/webapp/model/ContactType.java
@@ -0,0 +1,22 @@
+package com.urise.webapp.model;
+
+public enum ContactType {
+    PHONE("Home tel."),
+    MOBILE("Mobile"),
+    SKYPE("Skype"),
+    EMAIL("Email"),
+    LINKEDIN("Linkedin Profile"),
+    GITHUB("Githum profile"),
+    STACKOVERFLOW("Stackoverflow profile"),
+    HOME_PAGE("Personal page");
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
