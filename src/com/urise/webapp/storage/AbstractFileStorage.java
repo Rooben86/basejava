63ddad2b2e8086158f1f7aeedbcd9adaeb3bdeeb
ruben <rooben86@gmail.com>
2026-02-02 10:00:12 +0300

    hw9

--- src/com/urise/webapp/storage/AbstractFileStorage.java
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -24,7 +24,6 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
         this.directory = directory;
     }
 
-
     @Override
     protected File getSearchKey(String uuid) {
         return new File(directory, uuid);
@@ -74,7 +73,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     protected List<Resume> doCopyAll() {
         File[] files = directory.listFiles();
         if (files == null) {
-            throw new StorageException("Directory read error", null);
+            throw new StorageException("Directory read error");
         }
         List<Resume> list = new ArrayList<>(files.length);
         for (File file : files) {
@@ -97,7 +96,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     public int size() {
         String[] list = directory.list();
         if (list == null) {
-            throw new StorageException("Directory read error", null);
+            throw new StorageException("Directory read error");
         }
         return list.length;
     }
